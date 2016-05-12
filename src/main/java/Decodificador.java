da07260929f8d5755bebbcbffcbea65e7ee5f038
Juan Camilo Gaviria Holguín <juan.gaviria@ceiba.com.co>
2016-05-12 14:18:47 -0500

    Se soluciona error que lanzaba excepción al tratar de convertir un string muy largo "1000000..." en Integer

--- src/main/java/Decodificador.java
+++ src/main/java/Decodificador.java
@@ -1,27 +1,26 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Formatter;	
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Decodificador {
-	public static void main(String[] args) throws IOException, FileNotFoundException{
+	public static void main(String[] args) throws IOException, FileNotFoundException {
 		Scanner input = new Scanner(Paths.get("SETI_message_modified.txt"));
 		StringBuilder imagenUno = new StringBuilder();
 		StringBuilder imagenDos = new StringBuilder();
-		for(int contadorLinea = 1; contadorLinea < 2272; contadorLinea++) {
-			if(contadorLinea < 758) {
-				input.nextLine();
+		for (int contadorLinea = 1; contadorLinea <= 2271; contadorLinea++) {
+			if (contadorLinea <= 757) {
+				System.out.println(contadorLinea + ": " + input.nextLine());
 			}
-			if(contadorLinea >= 758 && contadorLinea < 1515) {
+			if (contadorLinea > 757 && contadorLinea <= 1514) {
 				StringBuilder linea = new StringBuilder(input.nextLine().replace(' ', '0'));
 				imagenUno.append(Integer.parseInt(linea.reverse().toString(), 2) + "\n");
 			}
-			if(contadorLinea >= 1515 && contadorLinea < 2272) {
+			if (contadorLinea > 1514 && contadorLinea <= 2271) {
 				StringBuilder linea = new StringBuilder(input.nextLine().replace(' ', '0'));
 				imagenDos.append(Integer.parseInt(linea.reverse().toString(), 2) + "\n");
 			}
-			contadorLinea++;
 		}
 		input.close();
 		Formatter archivoImagenUno = new Formatter("imagenUno.txt");
